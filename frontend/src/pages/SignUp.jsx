ca74ca4f45e0f4e8eb35170ad0665cdd28beb162
mihaivaidos <mihai@vaidos.com>
2025-05-28 19:57:57 +0300

    SignUp updated

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "../css/SignUp.css";
 import SignUpBox from "../components/SignUpBox";
-import { api } from '../config/api';
+import { API_URL } from '../config/api'; 
 
 export default function SignUp() {
   return (
     <div className="screen">
       <SignUpBox
         onSignUp={async (firstName, lastName, email, password) => {
-          try {
-            const data = await api.signup({ firstName, lastName, email, password });
-            alert(data.mes);
+            try {
+            const resp = await fetch(`${API_URL}/api/users`, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ firstName, lastName, email, password }),
+            });
+
+            const data = await resp.json(); 
+            if (!resp.ok) throw new Error(data.mes || "Server error");
           } catch (err) {
             alert(err.message);
           }
